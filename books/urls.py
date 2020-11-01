7b41439b75f53af21795d2ab3b00dfe94af8a4c1
Rafal <hajtpl@gmail.com>
2020-11-01 12:13:48 +0100

    Added create/update/delete views for book

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, reverse_lazy
 from django_filters.views import FilterView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 
 from books.models import Book
@@ -8,4 +8,7 @@ from books.filters import BookFilter
 
 urlpatterns = [
     path('book/list/', FilterView.as_view(filterset_class=BookFilter), name='book-list'),
+    path('book/create/', CreateView.as_view(model=Book, fields='__all__', success_url=reverse_lazy('books:book-list')), name='book-create'),
+    path('book/<int:pk>/edit/', UpdateView.as_view(model=Book, fields='__all__', success_url=reverse_lazy('books:book-list')), name='book-edit'),
+    path('book/<int:pk>/delete/', DeleteView.as_view(model=Book, success_url=reverse_lazy('books:book-list')), name='book-delete'),
 ]
