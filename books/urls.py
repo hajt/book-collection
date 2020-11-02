bcd3ad430817f38f564365e1dedee188e9c46cfc
Rafal <hajtpl@gmail.com>
2020-11-02 13:25:16 +0100

    Added views and templates for manage languages and authors

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,33 @@
-from django.urls import path, reverse_lazy
-from django_filters.views import FilterView
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import path
+
+from books.views import (
+    BookFilterListView, 
+    BookCreateView, 
+    BookUpdateView, 
+    BookDeleteView, 
+    AuthorListView, 
+    AuthorCreateView, 
+    AuthorUpdateView,
+    AuthorDeleteView,
+    LanguageListView, 
+    LanguageCreateView, 
+    LanguageUpdateView,
+    LanguageDeleteView,
+)
 
 
-from books.models import Book
-from books.filters import BookFilter
 
 urlpatterns = [
-    path('book/list/', FilterView.as_view(filterset_class=BookFilter), name='book-list'),
-    path('book/create/', CreateView.as_view(model=Book, fields='__all__', success_url=reverse_lazy('books:book-list')), name='book-create'),
-    path('book/<int:pk>/edit/', UpdateView.as_view(model=Book, fields='__all__', success_url=reverse_lazy('books:book-list')), name='book-edit'),
-    path('book/<int:pk>/delete/', DeleteView.as_view(model=Book, success_url=reverse_lazy('books:book-list')), name='book-delete'),
+    path('book/list/', BookFilterListView.as_view(), name='book-list'),
+    path('book/create/', BookCreateView.as_view(), name='book-create'),
+    path('book/<int:pk>/edit/', BookUpdateView.as_view(), name='book-edit'),
+    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('author/list/', AuthorListView.as_view(), name='author-list'),
+    path('author/create/', AuthorCreateView.as_view(), name='author-create'),
+    path('author/<int:pk>/edit/', AuthorUpdateView.as_view(), name='author-edit'),
+    path('author/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author-delete'),
+    path('language/list/', LanguageListView.as_view(), name='language-list'),
+    path('language/create/', LanguageCreateView.as_view(), name='language-create'),
+    path('language/<int:pk>/edit/', LanguageUpdateView.as_view(), name='language-edit'),
+    path('language/<int:pk>/delete/', LanguageDeleteView.as_view(), name='language-delete'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django_filters.views import FilterView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from books.models import Author, Book, Language
+from books.filters import BookFilter
+
+
+class BookFilterListView(FilterView):
+    filterset_class=BookFilter
+
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books:book-list') 
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books:book-list')
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books:book-list')
+
+
+class AuthorListView(ListView):
+    model = Author
+
+
+class AuthorCreateView(CreateView):
+    model = Author
+    fields = '__all__'
+    success_url = reverse_lazy('books:author-list') 
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    fields = '__all__'
+    success_url = reverse_lazy('books:author-list')
+
+
+class AuthorDeleteView(DeleteView):
+    model = Author
+    fields = '__all__'
+    success_url = reverse_lazy('books:author-list')
+
+
+class LanguageListView(ListView):
+    model = Language
+
+
+class LanguageCreateView(CreateView):
+    model = Language
+    fields = '__all__'
+    success_url = reverse_lazy('books:language-list') 
+
+
+class LanguageUpdateView(UpdateView):
+    model = Language
+    fields = '__all__'
+    success_url = reverse_lazy('books:language-list')
+
+
+class LanguageDeleteView(DeleteView):
+    model = Language
+    fields = '__all__'
+    success_url = reverse_lazy('books:language-list')
+    
