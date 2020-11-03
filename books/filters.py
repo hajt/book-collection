de9c379572301cc3de497d71e3421211483913f9
Rafal <hajtpl@gmail.com>
2020-11-03 22:16:10 +0100

    Improved filters

--- books/filters.py
+++ books/filters.py
@@ -1,16 +1,19 @@
-import django_filters
+import django_filters as filters
 
 from books.models import Book
 
 
-class BookFilter(django_filters.FilterSet):
+class BookFilter(filters.FilterSet):
 
-    publication_year = django_filters.RangeFilter()
+    authors__first_name = filters.CharFilter(lookup_expr='iexact', label="Author's first name")
+    authors__second_name = filters.CharFilter(lookup_expr='icontains', label="Author's other names")
+    authors__last_name = filters.CharFilter(lookup_expr='iexact', label="Author's last name")
+    publication_year = filters.RangeFilter(label="Publication year range")
+
 
     class Meta:
         model = Book
         fields = {
-            'title': ['contains'],
-            'authors': ['exact'],
+            'title': ['iexact', 'icontains'],
             'language': ['exact'],
             }
