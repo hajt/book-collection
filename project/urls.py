b89532c93f1186182ba712aceea098417f982a72
Rafal <hajtpl@gmail.com>
2020-11-04 21:02:09 +0100

    Added default redirect from main page

--- project/urls.py
+++ project/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',
+         RedirectView.as_view(pattern_name='books:book-list'),
+         name='dashboard'),
     path('collection/', include(('books.urls', 'books'), namespace='books')),
     path('api/', include('api.urls'))
 ]
