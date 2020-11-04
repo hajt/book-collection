391f9933972b44382852a7cc4d7254eea17b8321
Rafal <hajtpl@gmail.com>
2020-11-04 14:52:53 +0100

    Added validation passed external api URL

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 
 
+from urllib.parse import urlparse
+from django.core.exceptions import ValidationError
+
+
+def validate_googleapis_hostname(url):
+    """ Method for validation is hostname belongs to Google APIs. """
+    url = urlparse(url)
+    if url.hostname not in ['googleapis.com', 'www.googleapis.com']:
+        raise ValidationError('Only Google APIs URL allowed')
+
+
 class ApiImportForm(forms.Form):
     """ Form for passing External API URL. """
-    url = forms.URLField(label='External API URL', required=True)
+    url = forms.URLField(label='External API URL', required=True, validators=[validate_googleapis_hostname])
